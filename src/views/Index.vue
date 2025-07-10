a7f6bfcc0f2ab25ca94557bc62dc133bd7583a62
Liang.Cui <cuiliang0302@qq.com>
2025-07-10 20:16:22 +0800

    使用本地接口测试

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -11,7 +11,8 @@
 
 <script setup name="index">
 import { ref } from 'vue';
-const url = ref('https://umami.cuiliangblog.cn/share/0P4bgIxOLH8ckKuH/www.cuiliangblog.cn')
+const url = ref('http://192.168.10.100:3000/share/0P4bgIxOLH8ckKuH/www.cuiliangblog.cn')
+// const url = ref('https://umami.cuiliangblog.cn/share/0P4bgIxOLH8ckKuH/www.cuiliangblog.cn')
 </script>
 
 <style scoped>
